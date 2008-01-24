c7c6ad30d07e4be5cffbdb7c20fbb8d9c8fb4af0
acevery <acevery@a5642ea3-6e44-0410-a809-1769c79afe54>
2008-01-24 13:06:50 +0000

    add 0 frequence check

--- gphrase.py
+++ gphrase.py
@@ -18,7 +18,8 @@ total_nu=0
 
 #SEARCH_URL = "http://www.google.com/search?hl=en&lr=&as_qdr=all&%s&btnG=Search"
 #<b>1,060</b> for <b>allintext:
-RE = re.compile ("<b>([0-9\,]+)</b>项符合<b>")
+RE = re.compile (r"<b>([0-9\,]+)</b>项符合<b>")
+re_non = re.compile(r"<br>找不到和您的查询.*相符的网页。")
 
 #keepalive_handler = HTTPHandler ()
 #opener = urllib2.build_opener(keepalive_handler)
@@ -26,48 +27,59 @@ RE = re.compile ("<b>([0-9\,]+)</b>项符合<b>")
 # in key:(host,url)
 
 g_list=[\
-		("www.google.cn","http://www.google.cn/search?hl=zh_CN&c2coff=1&lr=&as_qdr=all&%s&btnG=Google+%s&meta="),\
-		("www.google.com.sg","http://www.google.com.sg/search?hl=zh_CN&c2coff=1&lr=&as_qdr=all&%s&btnG=Google+%s&meta"),\
-		("www.google.com","http://www.google.com/search?hl=zh_CN&c2coff=1&lr=&as_qdr=all&%s&btnG=Google+%s&meta=")]
+		#("www.google.cn","http://www.google.cn/search?hl=zh_CN&c2coff=1&lr=&as_qdr=all&%s&btnG=Google+%s&meta="),\
+		("www.google.cn","http://www.google.cn/search?hl=zh-CN&c2coff=1&%s&btnG=Google+%s&meta=&aq=f"),\
+		("www.google.com.sg","http://www.google.com.sg/search?hl=zh_CN&c2coff=1&lr=&as_qdr=all&%s&btnG=Google+%s&meta=&aq=f"),\
+		("www.google.com","http://www.google.com/search?hl=zh_CN&c2coff=1&lr=&as_qdr=all&%s&btnG=Google+%s&meta=&aq=f")]
 
 def get_search_result (keyword, g_tuple):
 	freq = -1
-	params0 = urllib.urlencode ({"q": "\"%s\"" % keyword})
-	params1 = urllib.pathname2url (keyword)
+	params0 = urllib.urlencode ({"as_epq": "\"%s\"" % keyword})
+	params1 = urllib.pathname2url ("搜索")
 	url = g_tuple[1] % (params0,params1)
 	req = urllib2.Request (url)
 	req.add_header ('User-agent', 'Mozilla/5.0')
 	req.add_header ('Host', g_tuple[0])
 	f = urllib2.urlopen (req)
-	for l in f:	
+	fetch=f.readlines()
+	for l in fetch:	
 		m = RE.findall (l)
 		if m:
 			freq = int (m[0].replace (",", ""))
 			break
+	if freq == -1:
+		m = re_non.findall(l)
+		if m:
+			freq = 0
 	return freq
 
 def process_phrase_file (name, total):
+#def process_phrase_file (name):
 
-	phrases = []
-	phrases_dict = {}
+#	phrases = []
+#	phrases_dict = {}
 	lines = []
 	result = False
 	
 	for l in open (name):
-		# we take a rest of 15s for every 100 times:)
-		if total % 500 ==0 and total != 0:
+		# we take a rest of 15s for every 1000 times:)
+		if total % 1000 ==0 and total != 0:
 			print 'It time to pause a bit :)'
-			time.sleep(15)
+		time.sleep(15)
 		phrase = l.strip ()
 		try:
+			# change search engine every 100 words
 			freq = get_search_result (phrase,g_list[ total/100%3 ])
 			if freq != -1:
 				result = True
+			else:
+				print >> sys.stderr, "During searching for \"%s\", we are banned from google. we should stop and try later." % phrase
+				sys.exit (-1)
+
+
 		except KeyboardInterrupt, e:
 			print >> sys.stderr, "Exit"
 			sys.exit (1)
-		except:
-			freq = -1
 		line = "%s\t%d" % (phrase, freq)
 		print line
 		lines.append (line)
@@ -112,5 +124,6 @@ if __name__ == "__main__":
 			continue
 		print "Start process " + fname
 		total_nu = process_phrase_file (fname, total_nu)
+		#process_phrase_file (fname)#, total_nu)
 		save_a_file_to_svn (fname + ".out")
 		print fname + " finished"
