50e9bc2a94696f1e649544571dd78ca44cb0794f
yufanyufan <yufanyufan@a5642ea3-6e44-0410-a809-1769c79afe54>
2008-01-29 06:40:56 +0000

    add more server

--- trunk/gphrase.py
+++ trunk/gphrase.py
@@ -34,7 +34,9 @@ g_list=[\
 		#("www.google.com","http://www.google.com/search?%s&hl=enN&newwindow=1&c2coff=1&lr=&nxpt=10.0827085840279239193376"),\
 		("72.14.235.147","http://72.14.235.147/search?%s&hl=zh_CN&newwindow=1&c2coff=1&lr=","http://72.14.235.147/search?%s&complete=1&hl=zh_CN&lr=&newwindow=1&c2coff=1&as_qdr=all&start=10&sa=N"),
 		("203.208.37.104","http://203.208.37.104/search?%s&hl=zh_CN&newwindow=1&c2coff=1&lr=","http://www.google.cn/search?%s&complete=1&hl=zh_CN&lr=&newwindow=1&c2coff=1&as_qdr=all&start=10&sa=N"),
-		("64.233.169.147","http://64.233.169.147/search?%s&hl=zh_CN&newwindow=1&c2coff=1&lr=","http://64.233.169.147/search?%s&complete=1&hl=zh_CN&lr=&newwindow=1&c2coff=1&as_qdr=all&start=10&sa=N")] #&nxpt=10.0827085840279239193376")]
+		("64.233.169.147","http://64.233.169.147/search?%s&hl=zh_CN&newwindow=1&c2coff=1&lr=","http://64.233.169.147/search?%s&complete=1&hl=zh_CN&lr=&newwindow=1&c2coff=1&as_qdr=all&start=10&sa=N"),
+		("66.249.89.147","http://64.233.169.147/search?%s&hl=zh_CN&newwindow=1&c2coff=1&lr=","http://66.249.89.147/search?%s&complete=1&hl=zh_CN&lr=&newwindow=1&c2coff=1&as_qdr=all&start=10&sa=N"),
+		("203.208.37.99","http://64.233.169.147/search?%s&hl=zh_CN&newwindow=1&c2coff=1&lr=","http://64.233.169.147/search?%s&complete=1&hl=zh_CN&lr=&newwindow=1&c2coff=1&as_qdr=all&start=10&sa=N")] #&nxpt=10.0827085840279239193376")]
 
 def fake_link (keyword,g_tuple):
 	param0 = urllib.urlencode ({"q": "\"%s\"" % keyword})
@@ -85,7 +87,7 @@ def process_phrase_file (name, total):
 		# we take a rest of 15s for every 1000 times:)
 		if total % 1000 ==0 and total != 0:
 			print 'It time to pause a bit :)'
-		sl_t = random.randint(3,12)/3.
+		sl_t = random.randint(3,12)/5.
 		print 'pause a while of %03f s' %sl_t
 		time.sleep(sl_t)
 		phrase = l.strip ()
@@ -95,7 +97,7 @@ def process_phrase_file (name, total):
 				# change search engine every 100 words
 		#		print phrase
 				while rd_n == _rd_n: 
-					rd_n = random.randint(0,2)
+					rd_n = random.randint(0,4)
 				_rd_n =rd_n
 				print rd_n
 				freq = get_search_result (phrase,g_list[ rd_n ])
@@ -145,7 +147,7 @@ def pick_a_file (files):
 
 def save_a_file_to_svn (file_name):
 	os.system ("svn add %s" % file_name)
-	os.system ("svn ci data -m \"add %s\"" % (file_name))
+	os.system ("svn ci data -m \"yufan add %s\"" % (file_name))
 
 if __name__ == "__main__":
 	#for keyword in sys.argv[1:]:
